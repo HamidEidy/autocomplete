f2a915d55f7be58ff627672a1317d293feb9f56d
Hamidreza Eidy <hamidreza.eidy1999@gmail.com>
2023-12-21 13:25:31 +0330

    Add files via upload

---
+++ AutoComplete.js
@@ -0,0 +1,36 @@
+(function () {
+  "use strict";
+  let inputField = document.getElementById('input');
+  let ulField = document.getElementById('suggestions');
+  
+  inputField.addEventListener('input', changeAutoComplete);
+  ulField.addEventListener('click', selectItem);
+
+  function changeAutoComplete({ target }) {
+    let data = target.value;
+    ulField.innerHTML = ``;
+    if (data.length) {
+      let autoCompleteValues = autoComplete(data);
+      autoCompleteValues.forEach(value => { addItem(value); });
+    }
+  }
+
+  function autoComplete(inputValue) {
+    let destination = ["لباسشویی", "ظرفشویی", "بخاری", "اتو"];
+    return destination.filter(
+      (value) => value.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  }
+
+  function addItem(value) {
+    ulField.innerHTML = ulField.innerHTML + `<li>${value}</li>`;
+  }
+
+  function selectItem({ target }) {
+    if (target.tagName === 'LI') {
+      inputField.value = target.textContent;
+      ulField.innerHTML = ``;
+    }
+  }
+})();
+
